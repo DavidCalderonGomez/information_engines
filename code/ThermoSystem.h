5307abfcfadb893d8c58bc27486a6f7b2a5892ea
david <dcalderon@unal.edu.co>
2024-12-17 01:38:31 -0500

    fixed code

--- code/ThermoSystem.h
+++ code/ThermoSystem.h
@@ -12,33 +12,32 @@ const double dt=ts/parameter;
 const double alpha = 1-exp(-gama*dt), alphap=alpha*(2-alpha);
 //---------------------------------------------------------------------------------------------------
 class Particle{
-  double Pos,Vel, Vi, Vp, Dv, Work=0, Measurement=0 ;
-  double Mass=0.8,Fex=0, Potential_zero = 0;//constant variables for each particle 
+  double Pos,Vel, Vi, Vp, Dv, Work, Measurement, Fex, Pot;
+  double Mass=0.8; 
  public:		    
   double get_Pos(){ return Pos; }
   double get_Vel(){ return Vel; }
-  double get_Potential_zero(){return Potential_zero;}
-  void set_Potential_zero(double value){Potential_zero=value;}
-  void reset(){Pos=0,Vel=0,Vi=0,Vp=0,Work=0,Measurement=0,Fex=0,Potential_zero=0;}
+  double get_Pot(){return Pot;}
+  void set_Pot(double value){Pot=value;}
   double get_Work(){return Work;}
   
-  void Initialize(double xi, double vi);
+  void Initialize(double Pos0, double Vel0);
   void Launch();//First step in the Gunsteren Berendsen algorithm
   void CalculateForce();//calculates all the forces acting on the particle
   void ThermoEvolution(Crandom &ran64);//evolves the system
-  double Protocol(double gain, Crandom &ran64,double previous_Pos, double SNR);//returns the difference between measurement and potential
+  double Protocol(double gain, Crandom &ran64, double SNR);//returns the difference between measurement and potential
   void CalculateWork(double previous_Pot);
 };
 //--------------------------------------------------------------------------------------------------
 //Method implementation
-void Particle::Initialize(double xi,double vi){
-  Pos=xi;Vi=vi; Vp=0; Dv=0;
+void Particle::Initialize(double Pos0, double Vel0){
+  Pos=Pos0,Vi=Vel0,Work=0,Pot=0;
 }
 void Particle::Launch(){
   Vel=Vi-dt*Fex/(2*Mass);
 }
 void Particle::CalculateForce(){ 
-  Fex = -kspring*(Pos-Potential_zero)-gravity*Mass;
+  Fex = -kspring*(Pos-Pot)-gravity*Mass;
 }
 void Particle::ThermoEvolution(Crandom &ran64){
   //one step of thermal evolution
@@ -47,14 +46,14 @@ void Particle::ThermoEvolution(Crandom &ran64){
   Pos=Pos+(Vel+Dv/2)*dt;
   Vel=Vp+Dv;
 }
-double Particle::Protocol(double gain, Crandom &ran64,double previous_Pos,double SNR){ //introduce some gain that ideally could be 2
-  Measurement = previous_Pos+(sqrt(kbT/kspring))/SNR*ran64.gauss(0,1); //This is a noisy measurement with SNR. 
-  double diff =Measurement-Potential_zero;
+double Particle::Protocol(double gain, Crandom &ran64,double SNR){ //introduce some gain that ideally could be 2
+  Measurement = Pos+(sqrt(kbT/kspring))/SNR*ran64.gauss(0,1); //This is a noisy measurement with SNR. 
+  double diff = Measurement-Pot;
   if(diff>0){ //if the measurement is over a the potential
     return gain*diff;// rise the potential
   }
   return 0;
 }
 void Particle::CalculateWork(double previous_Pot){
-  Work += kspring/2*(pow(Pos-Potential_zero,2.0)-pow(Pos-previous_Pot,2.0));
+  Work += kspring/2*(pow(Pos-Pot,2.0)-pow(Pos-previous_Pot,2.0));
 } //Estimating the work with the measurement as it was done in the experiment 
--- code/optimal.cpp
+++ code/optimal.cpp
@@ -5,46 +5,45 @@
 
 using std::cout, std::endl;
 
-
-int steps = parameter*samp_freq*100; 
-int runs = 100;
+int ttime = 100;
+int steps = parameter*samp_freq*ttime; 
+int runs = 1000;
 
 int main(){
   
   Crandom ran64(1);//create the generator of random numbers
   Particle particle[runs]; //create an array of particles  
-  double gain=0.5;double SNR=11;
-  double Pos=0, Pot=0;
-  int time=0;
-  double rise[runs]={};
-
+  double SNR=11;
+  double Pos=0, Pot=0, rise;
+  int t=0;
   double mean_work=0;
 
- for (gain = 0; gain <= 2; gain+=0.1){ //iterate over different gains
+ for (double gain = 1.62; gain <= 1.64; gain+=0.001){ //iterate over different gains
     
-    for(int k=0;k<runs;k++) particle[k].Initialize(0,0); 
-    for(int k=0;k<runs;k++) particle[k].Launch();
+    for(int k=0;k<runs;k++){
+      particle[k].Initialize(0,0); 
+      particle[k].CalculateForce();
+      particle[k].Launch();
+    } 
 
     for(int j=1;j<steps;j++){
         for(int k=0;k<runs;k++) {
-        
-	//evolve one step each particle:
+
         if(j%parameter==0){ 
-            Pot = particle[k].get_Potential_zero(); //store potential zero
+            Pot = particle[k].get_Pot(); //store potential zero
             Pos = particle[k].get_Pos(); //stores the position before the evolution        
-            rise[k] = particle[k].Protocol(gain,ran64,Pos,SNR);//calculate the one will be used in the next step
-            particle[k].set_Potential_zero(Pot+rise[k]);//change the potential with the result from the previous step
+            rise = particle[k].Protocol(gain,ran64,SNR);//calculate the one will be used in the next step
+            particle[k].set_Pot(Pot+rise);//change the potential with the result from the previous step
             particle[k].CalculateWork(Pot);//calculates the work with the past potential
             }	        
         particle[k].CalculateForce();//calculates all forces
-	    particle[k].ThermoEvolution(ran64);//evolves the system     
-    }
+	      particle[k].ThermoEvolution(ran64);//evolves the system     
+        }
     }
 
     for(int k=0;k<runs;k++) {
       mean_work+= particle[k].get_Work();
-      particle[k].reset();
-      }
+    }
     cout<<gain<<"   "<<mean_work/runs<<endl;
     mean_work=0;
     }
